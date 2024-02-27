3f2df2a4ff58aed8a092441446eb70ba73de09a6
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-27 15:52:51 +0000

    checking webhook request

--- src/routes/api/v1/messages/message-recipients/+server.js
+++ src/routes/api/v1/messages/message-recipients/+server.js
@@ -1,6 +1,5 @@
 /** @type {import('./$types').RequestHandler} */
-export async function POST(event) {
-	console.log('made it');
-	console.log(event);
+export async function POST({ request }) {
+	console.log(request.body);
 	return new Response();
 }
