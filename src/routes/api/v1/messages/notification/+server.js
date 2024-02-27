b237f97d40cd5d76df158593df99dbd6325ff87e
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-27 18:05:43 +0000

    Hunting bugs

--- src/routes/api/v1/messages/notification/+server.js
+++ src/routes/api/v1/messages/notification/+server.js
@@ -3,14 +3,16 @@ export async function POST({ locals: { supabase, supabase_admin }, request }) {
 	const body = await request.json();
 	const record = body.record;
 
-	const { data } = await supabase
-		.from('messages')
-		.select('subject, text, teams(name))')
-		.eq('id', record.message)
-		.single();
-	if (!data) return new Response(null, { status: 500 });
+	console.log('running');
 
-	console.log(data);
+	// const { data } = await supabase
+	// 	.from('messages')
+	// 	.select('subject, text, teams(name))')
+	// 	.eq('id', record.message)
+	// 	.single();
+	// if (!data) return new Response(null, { status: 500 });
+
+	// console.log(data);
 
 	// const html = ``;
 	// const subject = `New ${data.messages.teams.name} Message`;
