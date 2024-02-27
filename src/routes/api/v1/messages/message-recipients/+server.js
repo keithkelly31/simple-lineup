ac263c51a2fc1743afb224a7bd78f469b2c01cee
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-27 13:25:34 +0000

    Testing webhook logs

--- src/routes/api/v1/messages/message-recipients/+server.js
+++ src/routes/api/v1/messages/message-recipients/+server.js
@@ -1,4 +1,10 @@
 /** @type {import('./$types').RequestHandler} */
+export async function GET() {
+	console.log('testing');
+	return new Response();
+}
+
+/** @type {import('./$types').RequestHandler} */
 export async function POST(event) {
 	console.log(event);
 	return new Response();
