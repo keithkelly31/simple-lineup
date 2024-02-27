fbacb86d48dfbdd05bccfd69211790974b81526d
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-27 13:28:13 +0000

    Removed log testing

--- src/routes/api/v1/messages/message-recipients/+server.js
+++ src/routes/api/v1/messages/message-recipients/+server.js
@@ -1,10 +1,4 @@
 /** @type {import('./$types').RequestHandler} */
-export async function GET() {
-	console.log('testing');
-	return new Response();
-}
-
-/** @type {import('./$types').RequestHandler} */
 export async function POST(event) {
 	console.log(event);
 	return new Response();
