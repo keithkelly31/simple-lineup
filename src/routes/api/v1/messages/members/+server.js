cc5715264772c8def8a19588baa29f7a59c09f73
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-27 17:17:44 +0000

    Made id iteration promises

--- src/routes/api/v1/messages/members/+server.js
+++ src/routes/api/v1/messages/members/+server.js
@@ -5,17 +5,19 @@ export async function POST({ cookies, locals: { supabase_admin }, request }) {
 
 	if (record.message) return new Response();
 
-	const { error } = await supabase_admin
+	await supabase_admin
 		.from('message_members')
 		.insert({ message: record.id, member: record.member, read: true });
-	if (error) console.error(error);
 
-	record.ids
-		.split(',')
-		.forEach(
-			async (/** @type {string} */ id) =>
-				await supabase_admin.from('message_members').insert({ message: record.id, member: id })
-		);
+	async () => {
+		const promises = record.ids
+			.split(',')
+			.map(
+				async (/** @type {string} */ id) =>
+					await supabase_admin.from('message_members').insert({ message: record.id, member: id })
+			);
+		await Promise.all(promises);
+	};
 
 	return new Response();
 }
