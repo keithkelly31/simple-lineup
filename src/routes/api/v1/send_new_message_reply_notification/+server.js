6e3f18d9524ea9a1e4f42151a60f900f8767d924
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-27 21:39:50 +0000

    Checking reply

--- src/routes/api/v1/send_new_message_reply_notification/+server.js
+++ src/routes/api/v1/send_new_message_reply_notification/+server.js
@@ -8,6 +8,7 @@ export async function POST({ locals: { supabase_admin }, request, url }) {
 		.select('text, members(first_name, last_name), messages(subject, teams(id, name))')
 		.eq('id', record.id)
 		.single();
+	console.log(reply);
 	if (!reply)
 		return new Response(null, { status: 500, statusText: 'Problems getting the message reply' });
 
@@ -17,28 +18,28 @@ export async function POST({ locals: { supabase_admin }, request, url }) {
 		.eq('message', record.message)
 		.neq('member', record.member);
 
-	console.log(recipients);
 	if (!recipients)
 		return new Response(null, { status: 500, statusText: 'Problems getting the recipients' });
 
-	// const from = reply.messages.teams.name;
-	// const html = `
-	// 		<p>${reply.members.first_name} ${reply.members.last_name} has replied to the message <strong>${reply.messages.subject}</strong>.</p>
-	// 		<hr />
-	// 		<pre>${reply.text}</pre>
-	// 		<hr />
-	// 		<p>
-	// 			<a href="${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}">View and respond at SimpleLineup.com</a>
-	// 		</p>`;
-	// const subject = `New ${reply.messages.teams.name} Message Reply`;
-	// const text = `${reply.members.first_name} ${reply.members.last_name} has replied to the message ${reply.messages.subject}.\n\n${reply.text}\n\nView and respond at ${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}.`;
+	const from = reply.messages.teams.name;
+	const html = `
+			<p>${reply.members.first_name} ${reply.members.last_name} has replied to the message <strong>${reply.messages.subject}</strong>.</p>
+			<hr />
+			<pre>${reply.text}</pre>
+			<hr />
+			<p>
+				<a href="${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}">View and respond at SimpleLineup.com</a>
+			</p>`;
+	const subject = `New ${reply.messages.teams.name} Message Reply`;
+	const text = `${reply.members.first_name} ${reply.members.last_name} has replied to the message ${reply.messages.subject}.\n\n${reply.text}\n\nView and respond at ${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}.`;
 
 	const sendNotifications = async () => {
-		const promises = recipients.map(async (/** @type {any} */ r) => {
-			await supabase_admin
-				.from('notifications')
-				.insert({ from: '', html: '', member: r.id, subject: '', text: '' });
-		});
+		const promises = recipients.map(
+			async (/** @type {any} */ r) =>
+				await supabase_admin
+					.from('notifications')
+					.insert({ from, html, member: r.id, subject, text })
+		);
 		await Promise.all(promises);
 	};
 
