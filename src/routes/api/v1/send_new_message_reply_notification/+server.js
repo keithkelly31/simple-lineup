352b15526945ab85199646d69a9726fc343392a1
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-27 21:20:16 +0000

    Updated promises

--- src/routes/api/v1/send_new_message_reply_notification/+server.js
+++ src/routes/api/v1/send_new_message_reply_notification/+server.js
@@ -20,24 +20,23 @@ export async function POST({ locals: { supabase_admin }, request, url }) {
 		return new Response(null, { status: 500, statusText: 'Problems getting the recipients' });
 
 	const sendNotifications = async () => {
-		const from = reply.messages.teams.name;
-		const html = `
-		<p>${reply.members.first_name} ${reply.members.last_name} has replied to the message <strong>${reply.messages.subject}</strong>.</p>
-		<hr />
-		<pre>${reply.text}</pre>
-		<hr />
-		<p>
-			<a href="${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}">View and respond at SimpleLineup.com</a>
-		</p>`;
-		const subject = `New ${reply.messages.teams.name} Message Reply`;
-		const text = `${reply.members.first_name} ${reply.members.last_name} has replied to the message ${reply.messages.subject}.\n\n${reply.text}\n\nView and respond at ${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}.`;
+		const promises = recipients.map(async (/** @type {any} */ r) => {
+			const from = reply.messages.teams.name;
+			const html = `
+				<p>${reply.members.first_name} ${reply.members.last_name} has replied to the message <strong>${reply.messages.subject}</strong>.</p>
+				<hr />
+				<pre>${reply.text}</pre>
+				<hr />
+				<p>
+					<a href="${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}">View and respond at SimpleLineup.com</a>
+				</p>`;
+			const subject = `New ${reply.messages.teams.name} Message Reply`;
+			const text = `${reply.members.first_name} ${reply.members.last_name} has replied to the message ${reply.messages.subject}.\n\n${reply.text}\n\nView and respond at ${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}.`;
 
-		const promises = recipients.map(
-			async (/** @type {any} */ r) =>
-				await supabase_admin
-					.from('notifications')
-					.insert({ from, html, member: r.id, subject, text })
-		);
+			await supabase_admin
+				.from('notifications')
+				.insert({ from, html, member: r.id, subject, text });
+		});
 		await Promise.all(promises);
 	};
 
