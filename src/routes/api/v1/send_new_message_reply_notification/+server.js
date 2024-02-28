cea347c1c1274fcc931996a87a2dd9a3da9359e5
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-28 17:00:32 +0000

    Checking reply using view

--- src/routes/api/v1/send_new_message_reply_notification/+server.js
+++ src/routes/api/v1/send_new_message_reply_notification/+server.js
@@ -7,27 +7,27 @@ export async function POST({ locals: { supabase_admin }, request, url }) {
 	if (old_record.unread_reply === record.unread_reply) return new Response();
 
 	const { data: reply } = await supabase_admin
-		.from('message_replies')
-		.select(`*, members(first_name, last_name), messages(subject, teams(id, name))`)
+		.from('send_new_message_reply_notification')
+		.select()
 		.eq('id', record.id)
 		.single();
 	console.log(reply);
 	if (!reply)
 		return new Response(null, { status: 500, statusText: 'Problems getting the message reply' });
 
-	const from = reply.messages.teams.name;
-	const html = `
-			<p>${reply.members.first_name} ${reply.members.last_name} has replied to the message <strong>${reply.messages.subject}</strong>.</p>
-			<hr />
-			<pre>${record.text}</pre>
-			<hr />
-			<p>
-				<a href="${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}">View and respond at SimpleLineup.com</a>
-			</p>`;
-	const subject = `New ${reply.messages.teams.name} Message Reply`;
-	const text = `${reply.members.first_name} ${reply.members.last_name} has replied to the message ${reply.messages.subject}.\n\n${record.text}\n\nView and respond at ${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}.`;
+	// const from = reply.messages.teams.name;
+	// const html = `
+	// 		<p>${reply.members.first_name} ${reply.members.last_name} has replied to the message <strong>${reply.messages.subject}</strong>.</p>
+	// 		<hr />
+	// 		<pre>${record.text}</pre>
+	// 		<hr />
+	// 		<p>
+	// 			<a href="${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}">View and respond at SimpleLineup.com</a>
+	// 		</p>`;
+	// const subject = `New ${reply.messages.teams.name} Message Reply`;
+	// const text = `${reply.members.first_name} ${reply.members.last_name} has replied to the message ${reply.messages.subject}.\n\n${record.text}\n\nView and respond at ${url.origin}/team/${reply.messages.teams.id}/messages/${record.message}.`;
 
-	await supabase_admin.from('emails').insert({ from, html, member: record.member, subject, text });
+	// await supabase_admin.from('emails').insert({ from, html, member: record.member, subject, text });
 
 	return new Response();
 }
