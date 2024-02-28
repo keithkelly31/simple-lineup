bfbfc29ba794c08bd62ee7b3043e9fa642ed15cb
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-02-28 17:25:51 +0000

    Delete emails after sending

--- src/routes/api/v1/send_user_email/+server.js
+++ src/routes/api/v1/send_user_email/+server.js
@@ -1,11 +1,15 @@
 import { MAILGUN_DOMAIN } from '$env/static/private';
 
 /** @type {import('./$types').RequestHandler} */
-export async function POST({ locals: { mail, supabase }, request }) {
+export async function POST({ locals: { mail, supabase_admin }, request }) {
 	const body = await request.json();
 	const record = body.record;
 
-	const { data } = await supabase.from('members').select('email').eq('id', record.member).single();
+	const { data } = await supabase_admin
+		.from('members')
+		.select('email')
+		.eq('id', record.member)
+		.single();
 	if (!data) return new Response(null, { status: 500 });
 
 	const message = {
@@ -17,6 +21,8 @@ export async function POST({ locals: { mail, supabase }, request }) {
 	};
 
 	await mail.messages.create(MAILGUN_DOMAIN, message);
+
+	await supabase_admin.from('emails').delete().eq('id', record.id);
 
 	return new Response();
 }
