ba0b5aa1d5d788fed6e095f99f17d3b42c9207a3
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-03-12 18:10:28 +0000

    Still checking

--- src/routes/api/v1/stripe/customer/created/+server.js
+++ src/routes/api/v1/stripe/customer/created/+server.js
@@ -6,7 +6,7 @@ export async function POST({ locals: { stripe }, request }) {
 
 	let event;
 
-	console.log(request.body);
+	console.log(request.body?.json());
 
 	try {
 		event = stripe.webhooks.constructEvent(request.body, signature, STRIPE_SECRET_KEY);
