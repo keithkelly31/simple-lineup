3c081e9ebe1ec9651c60533230ca57a02391a067
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-03-12 18:36:40 +0000

    Try this again

--- src/routes/api/v1/stripe/customer/created/+server.js
+++ src/routes/api/v1/stripe/customer/created/+server.js
@@ -1,19 +1,7 @@
-import { STRIPE_SECRET_KEY } from '$env/static/private';
-
 /** @type {import('./$types').RequestHandler} */
 export async function POST({ locals: { stripe }, request }) {
-	const signature = request.headers.get('stripe-signature');
-
-	let event;
-
-	try {
-		event = stripe.webhooks.constructEvent(request.body, signature, STRIPE_SECRET_KEY);
-	} catch (error) {
-		// @ts-ignore
-		return new Response(`Webhook Error: ${error.message}`, { status: 400 });
-	}
-
-	console.log(event.data);
+	const data = await request.json();
+	console.log(data);
 
 	return new Response();
 }
