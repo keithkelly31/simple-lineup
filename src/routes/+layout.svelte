fe666f2238d07a4cdda463103913410e59b8a860
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-08-19 01:24:27 +0000

    Back to Pico

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,101 +1,30 @@
-<script>
-	import '@fontsource/roboto';
+<script lang="ts">
+	import type { Snippet } from 'svelte';
 	import '../app.scss';
-</script>
 
-<svelte:head>
-	<link
-		rel="stylesheet"
-		href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-	/>
-</svelte:head>
+	interface Props {
+		children: Snippet;
+	}
+
+	let { children }: Props = $props();
+</script>
 
 <header>
 	<nav>
 		<ul>
 			<li>
-				<a class="brand" href="/">Simple Lineup</a>
+				<strong>
+					<a href="/">Simple Lineup</a>
+				</strong>
 			</li>
 		</ul>
-
-		<!-- <ul>
-			<li><a href="/auth/signin">Sign In</a></li>
-			<li><a href="/auth/signup">Sign up</a></li>
-		</ul> -->
 	</nav>
 </header>
 
 <main>
-	<slot />
+	{@render children()}
 </main>
 
 <footer>
-	<nav>
-		<ul>
-			<li>&copy; Simple Lineup. All rights reserved.</li>
-		</ul>
-
-		<!-- <ul>
-			<li>
-				<a href="/privacy">Privacy</a>
-			</li>
-			<li>
-				<a href="/tos">Terms</a>
-			</li>
-		</ul> -->
-	</nav>
+	<div>© Simple Lineup. All rights reserved.</div>
 </footer>
-
-<style>
-	:root {
-		--error: var(--pico-color-red-500);
-		--success: var(--pico-color-green-550);
-
-		--pico-font-family: 'Roboto', var(--pico-font-family-sans-serif);
-	}
-
-	:global {
-		body {
-			display: flex;
-			flex-direction: column;
-			min-height: 100vh;
-		}
-
-		main {
-			flex: 1;
-		}
-
-		section {
-			margin-bottom: calc(var(--pico-block-spacing-vertical) * 3);
-		}
-
-		.material-symbols-outlined {
-			font-variation-settings:
-				'FILL' 0,
-				'wght' 400,
-				'GRAD' 0,
-				'opsz' 24;
-		}
-	}
-
-	.brand {
-		font-size: calc(var(--pico-font-size) * 1.25);
-		font-weight: 600;
-	}
-
-	/* header {
-		background-color: var(--pico-background-color);
-		border-bottom: var(--pico-border-width) solid var(--pico-muted-border-color);
-		left: 0;
-		padding: var(--pico-block-spacing-vertical) var(--pico-block-spacing-horizontal);
-		position: fixed;
-		top: 0;
-		width: 100vw;
-		z-index: 50;
-	}
-
-	main {
-		padding: calc(var(--pico-block-spacing-vertical) * 8) var(--pico-block-spacing-horizontal)
-			var(--pico-block-spacing-vertical) !important;
-	} */
-</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,22 +1,4 @@
 <script lang="ts">
-	import Form from '$lib/components/form.svelte';
-
-	let league: 'beer' | 'church' | 'rec' = $state('beer');
-
-	$effect(() => {
-		setInterval(() => {
-			switch (league) {
-				case 'beer':
-					league = 'church';
-					break;
-				case 'church':
-					league = 'rec';
-					break;
-				default:
-					league = 'beer';
-			}
-		}, 2000);
-	});
 </script>
 
 <svelte:head>
@@ -24,39 +6,17 @@
 	<meta name="description" content="" />
 </svelte:head>
 
-{#snippet leagues(league)}
-	<span>{league}</span>
-{/snippet}
-
-<section>
-	<h1>Simple {@render leagues(league)} league team manager.</h1>
-	<h2>Easily manage your roster, schedule, communications, and accounting.</h2>
-</section>
+<h1>Simple beer league team manager.</h1>
+<h2>Easily manage your roster, schedule, communications, and accounting.</h2>
 
 <h3>Sign up for updates</h3>
 <p>
 	This web app is currently in development. Sign up for email updates on the progress and more
 	information about features we are developing.
 </p>
-<Form label="Keep Me Updated">
+
+<form>
 	<label for="email">Email</label>
 	<input required autocomplete="email" id="email" name="email" type="email" />
-</Form>
-
-<style>
-	h2 {
-		color: var(--pico-muted-color);
-		font-size: 1rem;
-		font-weight: unset;
-	}
-
-	span {
-		background-color: var(--pico-muted-border-color);
-		border-radius: var(--pico-border-radius);
-		display: inline-block;
-		min-width: calc(var(--pico-font-size) * 4);
-		padding: calc(var(--pico-block-spacing-vertical) * 0.5)
-			calc(var(--pico-block-spacing-horizontal) * 0.5);
-		text-align: center;
-	}
-</style>
+	<button>Keep Me Updated</button>
+</form>
