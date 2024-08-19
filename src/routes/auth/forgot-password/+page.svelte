46ea0dae823bee5a9988a1a7ca3735cb1335f02f
Keith Kelly <5603878+keithkelly31@users.noreply.github.com>
2024-08-19 19:50:59 +0000

    Cleanout for Astro

--- src/routes/auth/forgot-password/+page.svelte
+++
@@ -1,8 +0,0 @@
-<h1>Forgot Password</h1>
-
-<form>
-	<label for="email">Email</label>
-	<input type="email" name="email" id="email" />
-
-	<button type="submit">Send Password Reset</button>
-</form>
